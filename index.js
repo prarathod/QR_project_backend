5102f814273382fa2f9536fa9ee87452e3d4e362
prarathod <itsmeprajwal2@gmail.com>
2023-03-25 15:07:40 +0530

    adding dotenv 1

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
-const conenctDB = require('./config/dbConnection');
+// const conenctDB = require('./config/dbConnection');
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use('/api/v1/dashboard', require('./routes/dashboard/dashboard.routes'));
 app.listen(port, ()=>{
-    conenctDB();
+    // conenctDB();
     console.log(`Server running on port ${port}`);
 })
